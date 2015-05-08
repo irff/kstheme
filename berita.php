a2c310d0ccc80696d3d919a986b9aa596ef8ee90
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-05-09 01:55:56 +0700

    Change berita page to new styles

--- berita.php
+++ berita.php
@@ -9,38 +9,48 @@ get_header(); ?>
 	get_template_part( 'parts/masthead' );
 ?>
 
-<section class="berita berita-page container">
+<section class="newberita container" id="berita">
 	<div class="row">
 		<div class="large-12 columns">
-			<h1>Berita Terbaru</h1>
+			<h1 data-sr>Berita Terbaru</h1>
 		</div>
 	</div>
 	<div class="row">
 		<div class="large-12 columns">
-			<ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2">
+			<ul class="small-block-grid-1 medium-block-grid-1 large-block-grid-3">
 				<?php
 				$args = Array('category_name' => 'berita',
 							  'posts_per_page' => 999);
+				$counter = 1;
 				$lastposts = get_posts($args);
 				foreach ( $lastposts as $post ) : setup_postdata( $post ); ?>
-					<li class="box">
+					<li class="box" data-sr>
 						<a href="<?php the_permalink(); ?>">
 							<div class="row">
-								<div class="small-4 medium-4 large-6 columns title-container">
-									<?php $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' )[0]; ?>
-									<div class="title">
-										<img src="<?php echo $featured_image; ?>" alt="">
-									</div>
+								<div class="large-6 column">
+									<?php the_time( get_option( 'date_format' ) ); ?>
 								</div>
-								<div class="small-8 medium-8 large-6 columns title-container">
-									<div class="title">
-										<h3><?php the_title(); ?></h3>										
-									</div>
+								<div class="large-6 column category">
+									<?php 
+										$category = get_the_category(); 
+										echo $category[0]->cat_name;
+									?>
 								</div>
+							</div>
+							<?php $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), "thumbnail" )[0]; ?>
+							<div class="image">
+								<img src="<?php echo $featured_image; ?>" alt="">
+							</div>
+							<div class="title">
+								<h3><?php the_title(); ?></h3>										
+							</div>
+							<div class="excerpt">
+								<?php the_excerpt(); ?>
 							</div>
 						</a>
 					</li>
 				<?php
+					$counter++;
 				endforeach; 
 				wp_reset_postdata(); ?>
 			</ul>			
