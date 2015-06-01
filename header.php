eda31f607b57171a4fca19f1eadb95868cbe958c
Tri Ahmad Irfan <triahmad1996@gmail.com>
2015-06-02 02:56:42 +0700

    Add Google Analytics tracking code

--- header.php
+++ header.php
@@ -24,6 +24,19 @@
 		<?php wp_head(); ?>
 	</head>
 	<body>
+
+	<!-- Google Analytics Script -->
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-59001195-4', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	
 	<?php do_action( 'foundationpress_after_body' ); ?>
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
